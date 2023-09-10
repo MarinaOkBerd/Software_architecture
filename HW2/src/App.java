284f290096a628652f4a30873d2b93f30f64b98d
Marina Berdnikova <berdni4ka@rambler.ru>
2023-09-10 13:35:49 +0300

    Add homework 2

--- HW2/src/App.java
+++ HW2/src/App.java
@@ -1,5 +1,8 @@
+import Core.Core;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Core core = new Core();
+        core.run();
     }
 }
