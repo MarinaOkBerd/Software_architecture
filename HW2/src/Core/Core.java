96c72e525c28484b9bde2ed8589a8458f0c513ea
Marina Berdnikova <berdni4ka@rambler.ru>
2023-09-12 12:45:33 +0300

    Add homework 2

--- HW2/src/Core/Core.java
+++ HW2/src/Core/Core.java
@@ -1,7 +1,24 @@
 package Core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import Abstractions.ItemGenerator;
+import Rewards.Bronze.BronzeGenerator;
+import Rewards.Gold.GoldGenerator;
+import Rewards.Silver.SilverGenerator;
+import Rewards.Stones.StonesGenerator;
+
 public class Core {
     public void run(){
-        
+       List<ItemGenerator> generatorList = new ArrayList<>();
+       generatorList.add(new GoldGenerator());
+       generatorList.add(new SilverGenerator());
+       generatorList.add(new BronzeGenerator());
+       generatorList.add(new StonesGenerator());
+       Random random = new Random();
+       ItemGenerator generator = generatorList.get(random.nextInt(0, generatorList.size()));
+       System.out.println(generator.openReward()); 
     }
 }
