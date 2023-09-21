38517891517544bc06455fd36a826867aa5b9f8f
Marina Berdnikova <berdni4ka@rambler.ru>
2023-09-22 02:11:22 +0300

    Add homework 5

---
+++ HW5/src/Data/CurrentTrafficRepository.java
@@ -0,0 +1,5 @@
+package Data;
+
+public class CurrentTrafficRepository implements ICurrentTrafficRepository{
+    
+}
---
+++ HW5/src/Data/RoadsRepository.java
@@ -0,0 +1,5 @@
+package Data;
+
+public class RoadsRepository implements ICityObjectRepository{
+    
+}
